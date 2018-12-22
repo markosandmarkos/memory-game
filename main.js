09364532f2a07221221de39322e7cdc38d7a92c3
stgrigoryan <stepan.grigoryan10@gmail.com>
2018-12-22 14:34:59 +0400

    Added some event handling

--- main.js
+++ main.js
@@ -8,10 +8,30 @@ window.onload = function() {
   for (let i = 0; i < length; i++) {
     let div = document.createElement('div');
     container[0].append(div);
-    div.className = ` node ${i}`;
-    var x = document.createElement("IMG");
-    x.src = double[i];
-    div.appendChild(x);
-    x.className = `image ${i}`;
-  };
+    div.className = `node ${i}`;
+    var image = document.createElement("IMG");
+    image.src = double[i];
+    div.appendChild(image);
+    image.className = `image ${i}`;
+    image.classList.add('hidden');
+
+  }
+
+  let nodes = document.getElementsByClassName('node');
+  let images = document.getElementsByClassName('image');
+
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].onclick = function () {
+      images[i].classList.remove('hidden');
+      for (let j = 0; j < nodes.length; j++) {
+        nodes[j].onclick = function () {
+          if (images[i].src != images[j].src) {
+            images[i].classList.add('hidden');
+          } else {
+            images[j].classList.remove('hidden');
+          }
+        };
+      }
+    };
+  }
 };
