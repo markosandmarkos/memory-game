4846d4c217203039d313f858b38edbf4388a12d6
Hasmik Hayrapetyan <h.hayrapetyan.92@gmail.com>
2019-01-19 00:08:07 +0400

    Change: var change let         game over animation         functional game images view         disable and enable buttons

--- public/js/script.js
+++ public/js/script.js
@@ -1,89 +1,138 @@
-var demoContainer = document.getElementById('demo_containers');
+let demoContainer = document.getElementById('demo_containers');
+let buttons = document.getElementsByClassName("disable-change");
+// let step = 1;
 
-var imgArray = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.png'];
-var imgArrayConcat = imgArray.concat(imgArray);
+let imgArray = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.png'];
+let imgArrayConcat = imgArray.concat(imgArray);
 
 imgArrayConcat = imgArrayConcat.sort(function(){ return 0.5-Math.random() });
 
-for (var i = 0; i < imgArrayConcat.length; i++) {
-	var dataVal = imgArrayConcat[i].split('.');
-	var html = '<div class="col-2 px-0">' +
-		'<img class="border bg-white p-2" width="100%" src="public/images/'+imgArrayConcat[i]+'">' +
-		'<input type="button" onclick="showImage(this)" data_value="'+dataVal[0]+'" class="position-absolute disable-change w-100 h-100 top-0 left-0 right-0">' +
-		'</div>';
+for (let i = 0; i < imgArrayConcat.length; i++) {
+	console.log(i);
+	let dataVal = imgArrayConcat[i].split('.');
+	let html = `<div class="col-2 px-0 node">
+		<img class="border bg-white p-2" width="100%" src="public/images/${imgArrayConcat[i]}" alt="image-${i}">
+		<input type="button" data-value="${dataVal[0]}" data-num="${i}" class="position-absolute disable-change w-100 h-100 top-0 left-0 right-0">
+		</div>`;
 	demoContainer.innerHTML += html;
 }
 
-// show images
-function showImage(e) {
-	e.previousSibling.classList.add('zoom-animation');
-	setTimeout(function(){
-		e.previousSibling.classList.remove('zoom-animation');
-	}, 1500);
+let imageIndexStore = "";
+let imageIndexNum = "";
+// let timer = 0;
+demoContainer.addEventListener("click",function (e) {
 
-	e.style.backgroundColor = 'unset';
-	// if (e.attributes.data_value.value >= 2){
-	// 	console.log(e.attributes.data_value.value);
-	// } else {
-	// 	console.log('no');
-	// }
-	// for (var i = 0; i < e.attributes.data_value.value.length; i++) {
-	// 	// console.log(e.attributes.data_value.value);
-	// }
+	if (!e.target) return;
+	/** @namespace e.target.dataset */
+	let {value} = e.target.dataset;
+	let {num} = e.target.dataset;
+
+	let b = e.target;
+
+	if (!b.classList.contains('unset')){
+		b.style.backgroundColor = 'unset';
+	}
+
+	if(imageIndexStore === ""){
+		imageIndexStore = value;
+		imageIndexNum = num;
+	}else if(imageIndexStore !== value) {
+        let a = imageIndexStore;
+        let indexNnum = imageIndexNum;
+		setTimeout(function () {
+			b.style.backgroundColor = '';
+			demoContainer.querySelector(`[data-value="${a}"][data-num="${indexNnum}"]`).style.backgroundColor = '';
+        }, 900);
+
+		setTimeout(function () {
+			imageIndexStore = "";
+			imageIndexNum = "";
+		},1000);
+
+	}else if(imageIndexStore !== "" && imageIndexStore === value) {
+		imageIndexStore = "";
+		imageIndexNum = "";
+	}
 
 
-}
+});
 
-var x;
+
+let x = null;
 function timeWork() {
 
 // Set the date we're counting down to
 
-	var dateNew = new Date();
-	var minutesNew = dateNew.getMinutes();
-	var hoursNew = dateNew.getHours();
-	var secondsNew = dateNew.getSeconds();
-	var monthNew = dateNew.getMonth() + 1;
-	var dayNew = dateNew.getDate();
-	var yearNew = dateNew.getFullYear();
+	let dateNew = new Date();
+	let minutesNew = dateNew.getMinutes();
+	let hoursNew = dateNew.getHours();
+	let secondsNew = dateNew.getSeconds();
+	let monthNew = dateNew.getMonth() + 1;
+	let dayNew = dateNew.getDate();
+	let yearNew = dateNew.getFullYear();
 	secondsNew = secondsNew;
-	minutesNew = minutesNew + 2;
-	var countDownDate = new Date(monthNew+" "+dayNew+", "+yearNew+" "+hoursNew+":"+minutesNew+":"+secondsNew).getTime();
+	minutesNew = minutesNew + 1;
+	let countDownDate = new Date(monthNew+" "+dayNew+", "+yearNew+" "+hoursNew+":"+minutesNew+":"+secondsNew).getTime();
 
 	// Update the count down every 1 second
 	x = setInterval(function() {
 
 		// Get todays date and time
-		var now = new Date().getTime();
+		let now = new Date().getTime();
 
 		// Find the distance between now and the count down date
-		var distance = countDownDate - now;
+		let distance = countDownDate - now;
 
 		// Time calculations for days, hours, minutes and seconds
-		var minutes = Math.floor((+distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((+distance % (1000 * 60)) / 1000);
+		let minutes = Math.floor((+distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((+distance % (1000 * 60)) / 1000);
 
 		// Display the result in the element with id="demo"
 		document.getElementById("countDown").innerHTML = minutes + ":" + seconds;
+		buttonsActiveDeactive(false);
 
 		// If the count down is finished, write some text
 		if (distance < 0) {
 			clearInterval(x);
-			document.getElementById("countDown").innerHTML = "game over";
+
+            document.getElementById('game_over').classList.add('scile');
+
+            setTimeout(function () {
+               document.getElementById('game_over').classList.remove('scile');
+            }, 18000);
+
+			document.getElementById("countDown").innerHTML = "00:00";
+            fullStartPosition();
 		}
 	}, 1000);
 
 }
 
 
+function fullStartPosition() {
+	setTimeout(function () {
+		for(let i = 0; i < buttons.length; i++) {
+			buttons[i].style.backgroundColor = '';
+		}
+		buttonsActiveDeactive(true);
+		document.getElementById('play').disabled = false;
+		document.getElementById('pause').disabled = false;
+	},1500)
+}
+
 // Buttons click disable and enable
 function buttonsActiveDeactive(e) {
-	var buttons = document.getElementsByClassName("disable-change");
-	for(var i = 0; i < buttons.length; i++) {
+
+	for(let i = 0; i < buttons.length; i++) {
 		buttons[i].disabled = e;
 		if (e === false) {
 			buttons[i].classList.add("cursor-p");
 		}else {
+			buttons[i].classList.remove("cursor-p");
+		}
+
+		if (buttons[i].style.backgroundColor === 'unset') {
+			buttons[i].disabled = true;
 			buttons[i].classList.remove("cursor-p");
 		}
 	}
@@ -91,22 +140,26 @@ function buttonsActiveDeactive(e) {
 buttonsActiveDeactive(true);
 
 // Play Game
-function playGame() {
+function playGame(e) {
+    e.disabled = true;
+    document.getElementById('pause').disabled = false;
 	buttonsActiveDeactive(false);
 	timeWork();
 }
 
 // Pause Game
 function pauseGame(e) {
+    e.disabled = true;
+    document.getElementById('play').disabled = false;
 	buttonsActiveDeactive(true);
 	clearInterval(x);
 }
 
 // Full screen function
 function toggleFullScreenMode() {
-    var btn = document.getElementById( 'btnFullScreenToggle' );
-    var parentBlock = document.getElementById("parentBlock");
-    var elem = document.documentElement; // html or root element
+    let btn = document.getElementById( 'btnFullScreenToggle' );
+	let parentBlock = document.getElementById("parentBlock");
+	let elem = document.documentElement; // html or root element
     if ( btn.value == 'goFull' ) {
         if (elem.requestFullscreen) {
   		  elem.requestFullscreen();
